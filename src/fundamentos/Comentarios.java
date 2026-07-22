7e3f52e77373f7276aef417e239579312ca5926c
douglasgvr <douglasfarmaangra@gmail.com>
2026-07-22 19:36:51 -0300

    feat: aprendendo sobre comentarios

---
+++ src/fundamentos/Comentarios.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+// Estamos usando uma classe. O que é isso?
+public class Comentarios {
+    /*
+    * O metodo main é a porta de entrada
+    * de uma aplicação Java. Quando estamos
+    * em um projeto real, basta ter um único
+    * método main dento do projeto */
+
+    static void main() {
+
+        // Exibe o texto String no console
+        System.out.println("Sistema Java");
+    }
+}
